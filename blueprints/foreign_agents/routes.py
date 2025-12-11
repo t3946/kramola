8bbc4f84f5c4d82033c8951a1f2e92cb8ca91dff
Vladislav <svialence@yandex.ru>
2025-12-11 16:35:01 +0300

    print change logs

--- blueprints/foreign_agents/routes.py
+++ blueprints/foreign_agents/routes.py
@@ -9,12 +9,18 @@ foreign_agents_bp = Blueprint('foreign_agents', __name__, template_folder='../..
 def fl():
     lp = ListPersons()
     words = lp.load()
+    changes = lp.get_changes_json()
+    # Sort changes by date (newest first)
+    changes_sorted = dict(sorted(changes.items(), key=lambda x: x[0], reverse=True))
 
-    return render_template('foreign-agents/fl.html', words=words)
+    return render_template('foreign-agents/fl.html', words=words, changes=changes_sorted)
 
 @foreign_agents_bp.route('/ul')
 def ul():
     lc = ListCompanies()
     words = lc.load()
+    changes = lc.get_changes_json()
+    # Sort changes by date (newest first)
+    changes_sorted = dict(sorted(changes.items(), key=lambda x: x[0], reverse=True))
 
-    return render_template('foreign-agents/ul.html', words=words)
+    return render_template('foreign-agents/ul.html', words=words, changes=changes_sorted)
