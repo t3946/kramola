21e9058651341ce529735b9dd032493eebd79cd7
falcon <werhcuh@gmail.com>
2025-12-30 15:58:29 +0300

    add word art test

--- tests/pdf/correctness/1-word-art/test_1word_art.py
+++ tests/pdf/correctness/1-word-art/test_1word_art.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from tests.pdf.correctness.base_pdf_test import BasePdfTest
 
 
-class TestOneWord(BasePdfTest):
+class TestOneWordArt(BasePdfTest):
     @staticmethod
     def test_find_words_in_pdf() -> None:
-        TestOneWord.run_test(Path(__file__))
+        TestOneWordArt.run_test(Path(__file__))
 
 
 if __name__ == '__main__':
-    TestOneWord.test_find_words_in_pdf()
+    TestOneWordArt.test_find_words_in_pdf()
