c89574bbdd0addb4d17a64af649da645f050bca1
falcon <werhcuh@gmail.com>
2026-01-15 22:25:46 +0300

    use justvalidate package for validation

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -4,5 +4,6 @@
 import './MainApp.js';
 import './ErrorPopup.js';
 import './components/ProgressBar.js';
+import './components/ui/InputFile.js';
+import './components/analyse-form/AnalyseForm.js';
 import './pages/Highlight.js';
-import './pages/HighlightUploadPage.js';
