14434c49d664db0f9b944c41921e4bb4a342002d
falcon <werhcuh@gmail.com>
2026-02-05 00:42:20 +0300

    specify list

--- tests/docx/profanity_words_1/profanity_words_1.py
+++ tests/docx/profanity_words_1/profanity_words_1.py
@@ -1,9 +1,13 @@
 from pathlib import Path
+from typing import List
 
+from services.words_list import PredefinedListKey
 from tests.docx.base_docx_test import BaseDocxTest
 
 
 class TestProfanityWords(BaseDocxTest):
+    predefined_lists: List[PredefinedListKey] = [PredefinedListKey.PROFANITY]
+
     @staticmethod
     def test_find_profanity_in_docx() -> None:
         TestProfanityWords.run_test(Path(__file__))
