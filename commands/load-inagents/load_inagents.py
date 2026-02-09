aac0c18d7014f72ed1da17e009787a20caa6740f
falcon <werhcuh@gmail.com>
2026-02-09 23:50:50 +0300

    load inagents

---
+++ commands/load-inagents/load_inagents.py
@@ -0,0 +1,4 @@
+1. открыть https://minjust.gov.ru/ru/pages/reestr-inostryannykh-agentov/#
+2. Выполнить getLink.js, результатом будет null или строка (ссылка)
+3. Если результат был ссылка, тогда загрузить файл по этой ссылке и сохранить в папку temp.
+
