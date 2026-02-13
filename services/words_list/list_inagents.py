cba6641550179e90bb6eb268f6fd07e626d26965
falcon <werhcuh@gmail.com>
2026-02-13 16:27:54 +0300

    update filter

--- services/words_list/list_inagents.py
+++ services/words_list/list_inagents.py
@@ -1,5 +1,7 @@
 from typing import ClassVar, List
 
+from sqlalchemy import or_
+
 from models import Inagent
 from models.inagents import AgentType
 from services.fulltext_search.phrase import Phrase
@@ -15,6 +17,18 @@ class ListInagents:
 
         if self.agent_types:
             query = query.filter(Inagent.agent_type.in_(self.agent_types))
+
+        # [start] filter only active inagents
+        i_d = Inagent.include_minjust_date
+        e_d = Inagent.exclude_minjust_date
+        # only "Числится": (i_d and not e_d) or (i_d and e_d and i_d > e_d)
+        query = query.filter(
+            or_(
+                (i_d.isnot(None)) & (e_d.is_(None)),
+                (i_d.isnot(None)) & (e_d.isnot(None)) & (i_d > e_d),
+            )
+        )
+        # [end]
 
         rows = query.all()
         merged: list[str] = []
