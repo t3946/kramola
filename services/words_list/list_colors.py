b10a9c7317d397af996cd48aed29d8451d504066
falcon <werhcuh@gmail.com>
2026-02-25 10:30:36 +0300

    inherit highlight color

--- services/words_list/list_colors.py
+++ services/words_list/list_colors.py
@@ -1,7 +1,17 @@
 from models.phrase_list.list_record import ListRecord
+from services.utils.color import Color
 
 
 class ListColor:
+    highlight_color: Color
+    key: str
+
+    def __init__(self, *args, **kwargs):
+        record = ListRecord.query.filter_by(slug=self.key).first()
+        self.highlight_color = Color(record.color)
+
+        super().__init__(*args, **kwargs)
+
     def get_color(self) -> str:
         return (
             ListRecord
