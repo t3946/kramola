40e3aaeea09594425d103647ba53f61882fb368a
falcon <werhcuh@gmail.com>
2026-02-28 10:30:06 +0300

    simplify

--- services/parser_feds_fm/parser_feds_fm.py
+++ services/parser_feds_fm/parser_feds_fm.py
@@ -1,8 +1,7 @@
-import re
 import json
 from pathlib import Path
 from extensions import db
-from typing import List, Union, Dict, Tuple
+from typing import List, Dict, Tuple
 
 from models.extremists_terrorists import ExtremistArea, ExtremistStatus, ExtremistTerrorist
 from services.parser_feds_fm.process_raw import ProcessRaw
@@ -126,48 +125,26 @@ class ParserFedsFM (ProcessRaw):
 
         # [start] process raw data to rich data objects
         # international all FL/UL: raw + sanction_code; russian FL: raw + birth_date; russian UL: raw only
-        rich_data: dict = {
-            "international": {"all": {}, "excluded": {}},
-            "russian": {"all": {}, "excluded": {}},
-        }
+        rich_data: List[dict] = []
 
-        rich_data["international"]["all"]["namesFL"] = [
-            {"raw": s, "sanction_code": self._parse_sanction_code(s)}
-            for s in raw_data["international"]["all"]["namesFL"]
-        ]
+        rich_data.extend([{"raw": s, "type": ExtremistStatus.FIZ, "area": ExtremistArea.INTERNATIONAL, "is_active": True} for s in raw_data["international"]["all"]["namesFL"]])
+        rich_data.extend([{"raw": s, "type": ExtremistStatus.UR, "area": ExtremistArea.INTERNATIONAL, "is_active": True} for s in raw_data["international"]["all"]["namesUL"]])
+        rich_data.extend([{"raw": s, "type": ExtremistStatus.FIZ, "area": ExtremistArea.INTERNATIONAL, "is_active": False} for s in raw_data["international"]["excluded"]["namesFL"]])
+        rich_data.extend([{"raw": s, "type": ExtremistStatus.UR, "area": ExtremistArea.INTERNATIONAL, "is_active": False} for s in raw_data["international"]["excluded"]["namesUL"]])
+        rich_data.extend([{"raw": s, "type": ExtremistStatus.FIZ, "area": ExtremistArea.RUSSIAN, "is_active": True} for s in raw_data["russian"]["all"]["namesFL"]])
+        rich_data.extend([{"raw": s, "type": ExtremistStatus.UR, "area": ExtremistArea.RUSSIAN, "is_active": True} for s in raw_data["russian"]["all"]["namesUL"]])
+        rich_data.extend([{"raw": s, "type": ExtremistStatus.FIZ, "area": ExtremistArea.RUSSIAN, "is_active": False} for s in raw_data["russian"]["excluded"]["namesFL"]])
+        rich_data.extend([{"raw": s, "type": ExtremistStatus.UR, "area": ExtremistArea.RUSSIAN, "is_active": False} for s in raw_data["russian"]["excluded"]["namesUL"]])
+        # [end]
 
-        rich_data["international"]["all"]["namesUL"] = [
-            {"raw": s, "sanction_code": self._parse_sanction_code(s)}
-            for s in raw_data["international"]["all"]["namesUL"]
-        ]
+        # [start] hydrate rich data objects
+        for item in rich_data:
+            if item["area"] == ExtremistArea.INTERNATIONAL:
+                item["sanction_code"] = self._parse_sanction_code(item["raw"])
 
-        rich_data["international"]["excluded"]["namesFL"] = [
-            {"raw": s, "sanction_code": self._parse_sanction_code(s)}
-            for s in raw_data["international"]["excluded"]["namesFL"]
-        ]
-
-        rich_data["international"]["excluded"]["namesUL"] = [
-            {"raw": s, "sanction_code": self._parse_sanction_code(s)}
-            for s in raw_data["international"]["excluded"]["namesUL"]
-        ]
-
-        rich_data["russian"]["all"]["namesFL"] = [
-            {"raw": s, "birth_date": self._parse_birthdate(s)}
-            for s in raw_data["russian"]["all"]["namesFL"]
-        ]
-
-        rich_data["russian"]["all"]["namesUL"] = [
-            {"raw": s} for s in raw_data["russian"]["all"]["namesUL"]
-        ]
-
-        rich_data["russian"]["excluded"]["namesFL"] = [
-            {"raw": s, "birth_date": self._parse_birthdate(s)}
-            for s in raw_data["russian"]["excluded"]["namesFL"]
-        ]
-
-        rich_data["russian"]["excluded"]["namesUL"] = [
-            {"raw": s} for s in raw_data["russian"]["excluded"]["namesUL"]
-        ]
+            if item["area"] == ExtremistArea.RUSSIAN and item["type"] == ExtremistStatus.FIZ:
+                item["birth_date"] = self._parse_birthdate(item["raw"])
+                item["names"] = self._parse_ru_fl_name(item["raw"])
         # [end]
 
         return
