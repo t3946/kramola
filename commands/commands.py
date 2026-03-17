d6b29c8b557deebe10bc53b15ffff356e3f773bd
falcon <werhcuh@gmail.com>
2026-03-17 13:22:43 +0300

    format inagents

--- commands/commands.py
+++ commands/commands.py
@@ -5,6 +5,7 @@ from flask import Flask
 from commands.create_admin import create_admin_cmd
 from commands.load_inagents_cmd import load_inagents_cmd
 from commands.parse_inagents_cmd import parse_inagents_cmd
+from commands.strip_inagents_search_terms_initials import strip_inagents_search_initials_cmd
 from commands.task_result_cmd import task_result_cmd, task_stats_cmd
 from commands.update_inagents_cmd import update_inagents_cmd
 from commands.parse_extremists import sync_extremists_cmd
@@ -14,6 +15,7 @@ def register_commands(app: Flask) -> None:
     app.cli.add_command(create_admin_cmd)
     app.cli.add_command(load_inagents_cmd)
     app.cli.add_command(parse_inagents_cmd)
+    app.cli.add_command(strip_inagents_search_initials_cmd)
     app.cli.add_command(task_result_cmd)
     app.cli.add_command(task_stats_cmd)
     app.cli.add_command(update_inagents_cmd)
