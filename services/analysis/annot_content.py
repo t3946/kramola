336c0cbef4c90a868368787f647a27a00921672f
falcon <werhcuh@gmail.com>
2026-03-25 18:22:42 +0300

    modify footnotes output

--- services/analysis/annot_content.py
+++ services/analysis/annot_content.py
@@ -1,13 +1,15 @@
 """Shared logic for annotation/comment title and content from AnalysisMatch."""
 
-from typing import Tuple, List,Union
+from typing import Tuple, List, Union, Optional
 
 from models import Inagent
 from models.inagents import AgentType
 from services.analysis.analysis_match import AnalysisMatch
 from services.enum.predefined_list import ESearchSourceAnnotTitle
 from services.fulltext_search import Phrase
-from services.fulltext_search.search_match import FTSTextMatch, FTSRegexMatch
+from services.fulltext_search.search_match import FTSTextMatch, FTSRegexMatch, FTSMatch
+from services.words_list import WordsList
+from services.words_list.list_profanity import ListProfanity
 
 
 def get_annot_title_content(match: AnalysisMatch) -> Tuple[str, str]:
@@ -57,6 +59,40 @@ def get_annot_title_content(match: AnalysisMatch) -> Tuple[str, str]:
 
 
 def get_multiple_get_annot_title_content(matches: List[AnalysisMatch]) -> Tuple[str, str]:
+    # [start] reduce profanity matches
+    filtered_matches: List[AnalysisMatch] = []
+    profanity_included = False
+
+    for match in matches:
+        fts_match: FTSMatch = match.search_match
+        source_list: Optional[WordsList] = None
+
+        if isinstance(fts_match, FTSTextMatch):
+            phrase: Phrase = fts_match.search_phrase
+
+            if phrase is None:
+                continue
+
+            source_list = phrase.source_list
+        elif isinstance(fts_match, FTSRegexMatch):
+            source_list = fts_match.regex_info.source_list
+
+        # take only one profanity in filtered matches
+        if source_list and isinstance(source_list, ListProfanity):
+            if profanity_included:
+                continue
+
+            profanity_included = True
+
+        filtered_matches.append(match)
+
+    matches = filtered_matches
+    # [end]
+
+    # simplify output if possible
+    if len(matches) == 1:
+        return get_annot_title_content(matches[0])
+
     title = f"{len(matches)} совпадений"
     content = ""
 
